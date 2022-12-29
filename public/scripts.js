27e0da65d43c60d9ce62825139986c38cb34e5e9
justinhwbhang <justinhwbhang@gmail.com>
2022-12-28 17:50:17 -0800

    signup button linked to backend
    basic logic for http requests from frontend to backend api server set up

--- public/scripts.js
+++ public/scripts.js
@@ -2,3 +2,11 @@
 $(document).ready(function(){
     $('.modal-trigger').leanModal();
   });
+  
+function signup()
+{
+  console.log("Hello World")
+  fetch('http://localhost:3000/')
+  .then((response) => response.text())
+  .then((data) => console.log(data));
+}
