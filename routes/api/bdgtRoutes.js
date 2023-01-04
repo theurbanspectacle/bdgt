a8197e19961a09fc4054b51d72c2dc8c0e712e99
Bryan Smith <smitbry17@gmail.com>
2023-01-04 14:28:14 -0800

    added purchase code and income code to budget routes

--- routes/api/bdgtRoutes.js
+++ routes/api/bdgtRoutes.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
-// TODO: Add BDGT API ROUTES 
+// TODO: Add BDGT API ROUTES
+router.post("/purchase", (req, res) => {
+  if (req.session.user) {
+    savePurchase(req.body.purchase, req.session.user.id);
+    res.send("Purchase Saved!");
+  } else {
+    res.status(401).json({ message: "Unable to save purchase" });
+    return;
+  }
+});
+router.post("/income", (req, res) => {});
 
 module.exports = router;
