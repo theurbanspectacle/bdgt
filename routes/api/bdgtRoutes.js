0aaa406c97f18d474faa7a38e4d44f299554c6bb
Bryan Smith <smitbry17@gmail.com>
2023-01-04 15:48:01 -0800

    Got database populated for handling price and budget

--- routes/api/bdgtRoutes.js
+++ routes/api/bdgtRoutes.js
@@ -1,7 +1,13 @@
+const Purchase = require("../../lib/purchase");
+const User = require("../../lib/User");
 const router = require("express").Router();
 
 // TODO: Add BDGT API ROUTES
 router.post("/purchase", (req, res) => {
+  Purchase.create({
+    price: req.body.price,
+  });
+
   if (req.session.user) {
     savePurchase(req.body.purchase, req.session.user.id);
     res.send("Purchase Saved!");
